b0da831af13ab0f8370283bfc3be823ea20c4898
zhangsht <zhsht201501102104@gmail.com>
2017-05-07 21:28:08 +0800

    回溯与队列

--- src/algorithm/BSTDelete.java
+++ src/algorithm/BSTDelete.java
@@ -1,15 +1,5 @@
 package algorithm;
 
-class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
-
-	TreeNode(int x) {
-		val = x;
-	}
-}
-
 public class BSTDelete {
 	public TreeNode deleteNode(TreeNode root, int key) {
 		if (root == null) return null;
