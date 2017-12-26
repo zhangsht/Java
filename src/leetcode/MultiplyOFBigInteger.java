f51fc0a48d7a322670a7d2359e5eb9bb7d8dbf67
zhangsht <zhsht201501102104@gmail.com>
2017-12-26 16:14:29 +0800

    multiply of BigInteger

--- src/leetcode/MultiplyOFBigInteger.java
+++ src/leetcode/MultiplyOFBigInteger.java
@@ -3,8 +3,32 @@ package leetcode;
 public class MultiplyOFBigInteger {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		MultiplyOFBigInteger multiplyOFBigInteger = new MultiplyOFBigInteger();
+		String num1 = "12";
+		String num2 = "12";
+		String result = multiplyOFBigInteger.multiply(num1, num2);
+		System.out.println(result);
+	}
+	
+	public String multiply(String num1, String num2) {
+		int m = num1.length();
+		int n = num2.length();
+		int [] pos = new int[m + n];
+		
+		for (int i = m - 1; i >= 0; i--) {
+			for (int j = n - 1; j >= 0; j--) {
+				int mul = (num1.charAt(i) - '0') * (num2.charAt(j) - '0') + pos[i + j + 1];
+				pos[i + j] += mul / 10;
+				pos[i + j + 1] = mul % 10;
+			}
+		}
+		StringBuilder sb = new StringBuilder();
+		for (int a : pos) {
+			if (!(sb.length() == 0 && a == 0)) {
+				sb.append(a);
+			}
+		}
+		return sb.length() == 0 ? "0" : sb.toString();
 	}
 
 }
