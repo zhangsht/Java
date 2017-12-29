07472d0b2772c2ff6f44ac8926dd1b5856d587d2
zhangsht <zhsht201501102104@gmail.com>
2017-12-29 09:38:45 +0800

    combination sum 2: number is only used once

--- src/leetcode/CombinationSum.java
+++ src/leetcode/CombinationSum.java
@@ -8,9 +8,20 @@ public class CombinationSum {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		CombinationSum combinationSum = new CombinationSum();
+		int candidates[] = { 10, 1, 2, 7, 6, 8, 1, 5 };
+		int target = 8;
+		List<List<Integer>> list = combinationSum.combinationSum2(candidates, target);
+		for (List<Integer> row : list) {
+			for (int a : row) {
+				System.out.print(a + " ");
+			}
+			System.out.println();
+		}
+		System.out.println("done");
 	}
 
+	// number may be used more than one 
 	public List<List<Integer>> combinationSum(int[] candidates, int target) {
 		if (candidates == null) {
 			return null;
@@ -29,6 +40,7 @@ public class CombinationSum {
 			for (int i = start; i < candidates.length; i++) {
 				// backtracking
 				cur.add(candidates[i]);
+				// next index remains i
 				getResult(candidates, target - candidates[i], i, list, cur);
 				cur.remove(cur.size() - 1);
 			}
@@ -36,4 +48,30 @@ public class CombinationSum {
 		return;
 	}
 
+	// number is only used once 
+	public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+		if (candidates == null) {
+			return null;
+		}
+		Arrays.sort(candidates);
+		List<List<Integer>> list = new ArrayList<>();
+		getResult2(candidates, target, 0, list, new ArrayList<Integer>());
+		return list;
+	}
+
+	private void getResult2(int[] candidates, int target, int start, List<List<Integer>> list, ArrayList<Integer> cur) {
+		if (target == 0) {
+			// add a new list
+			list.add(new ArrayList<>(cur));
+		} else if (target > 0) {
+			for (int i = start; i < candidates.length; i++) {
+				// backtracking
+				cur.add(candidates[i]);
+				// next index need adding 1
+				getResult2(candidates, target - candidates[i], i + 1, list, cur);
+				cur.remove(cur.size() - 1);
+			}
+		}
+		return;
+	}
 }
