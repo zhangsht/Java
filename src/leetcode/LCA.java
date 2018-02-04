cc257600195954b74bb709cc77a425bf5d34ddd0
zhangsht <zhangsht95@qq.com>
2018-02-05 07:15:28 +0800

    LCA II

--- src/leetcode/LCA.java
+++ src/leetcode/LCA.java
@@ -12,7 +12,23 @@ public class LCA {
 		while ((root.val - p.val) * (root.val - q.val) > 0) {
 			root = p.val < root.val ? root.left : root.right;
 		}
-		
+
 		return root;
+	}
+
+	/*
+	 * find the lowest common ancestor (LCA) of two given nodes in the tree.
+	 */
+	public TreeNode lowestCommonAncestor2(TreeNode root, TreeNode p, TreeNode q) {
+		if (root == null || root == p ||root == q) {
+			return root;
+		}
+		// iteration
+		TreeNode left = lowestCommonAncestor(root.left, p, q);
+		TreeNode right = lowestCommonAncestor(root.right, p, q);
+		if (left != null && right != null) {
+			return root;
+		}
+		return left == null ? right : left;
 	}
 }
